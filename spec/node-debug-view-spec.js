db8c29efddfbb89875604d88bce7bf2b629eaacc
pstromberg98 <pstromberg@nextcenturymeters.com>
2019-03-27 12:46:35 -0600

    Init commit

---
+++ spec/node-debug-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NodeDebugView from '../lib/node-debug-view';
+
+describe('NodeDebugView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
